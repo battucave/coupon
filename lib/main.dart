4c4adba1ea7e81578d04e4c1bf9b6b74d00eea7f
shoaibakhtar57 <shoaib.dev57@gmail.com>
2022-12-13 20:55:58 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ void main() {
   // SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
   //   statusBarColor:  KColor.blueGreen, // status bar color
   // ));
-  //update
   SystemChrome.setPreferredOrientations(
     [
       DeviceOrientation.portraitUp,
