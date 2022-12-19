2c9c9df8d44e89475c7a486419dbbf8b42f79e55
shoaibakhtar57 <shoaib.dev57@gmail.com>
2022-12-19 20:39:35 +0500

    update

--- lib/controllers/subscription_controller.dart
+++ lib/controllers/subscription_controller.dart
@@ -10,7 +10,6 @@ import 'package:logan/constant/api_routes.dart';
 import 'package:logan/controllers/builder_ids/builder_ids.dart';
 import 'package:logan/network_services/network_handler.dart';
 import 'package:logan/views/global_components/k_bottom_navigation_bar.dart';
-import 'package:logan/views/screens/auth/login_screen.dart';
 
 import '../models/subscription/purchasable_product.dart';
 
@@ -26,10 +25,6 @@ class SubscriptionController extends GetxController {
 
   bool isLoading = false;
 
-  // static const String storeKeyConsumable = 'storeKeyConsumable';
-  // static const String storeKeySubscription = 'storeKeySubscription';
-  // static const String storeKeyUpgrade = 'storeKeyUpgrade';
-
   void initialize() async {
     final purchaseUpdate = iaPurchase.purchaseStream;
     subscription = purchaseUpdate.listen(
@@ -42,18 +37,7 @@ class SubscriptionController extends GetxController {
 
   Future<void> buy(ProductDetails product) async {
     final purchaseParam = PurchaseParam(productDetails: product);
-    // switch (product.id) {
-    //   case storeKeyConsumable:
-    //     await iaPurchase.buyConsumable(purchaseParam: purchaseParam);
-    //     break;
-    //   case storeKeySubscription:
-    //   case storeKeyUpgrade:
     iaPurchase.buyNonConsumable(purchaseParam: purchaseParam);
-    //     break;
-    //   default:
-    //     throw ArgumentError.value(
-    //         product, '${product.id} is not a known product');
-    // }
   }
 
   void _onPurchaseUpdate(List<PurchaseDetails> purchaseDetailsList) {
@@ -63,23 +47,14 @@ class SubscriptionController extends GetxController {
   Future<void> _handlePurchase(PurchaseDetails purchaseDetails) async {
     log(purchaseDetails.status.toString());
     if (purchaseDetails.status == PurchaseStatus.purchased) {
-      //TODO: Verify purchase from server
-
-      // var validPurchase = await _verifyPurchase(purchaseDetails);
-      // validPurchase = true;
-
-      //Valid purchase condition when purchase verified from backend
-      // if (validPurchase) {
-      // Apply changes locally
-      // switch (purchaseDetails.productID) {
-      //   case storeKeySubscription:
-      String serverKey =
-          purchaseDetails.verificationData.serverVerificationData;
-      log('LOCAL VERIFICATION::: ${purchaseDetails.verificationData.localVerificationData}');
-      log('SERVER VERIFICATION:::: ${purchaseDetails.verificationData.serverVerificationData}');
-      log('TRANSACTION DATE:::; ${purchaseDetails.transactionDate.toString()}');
-      log('PRODUCT ID::::;; ${purchaseDetails.productID}');
-      log('PRODUCT ID::::;; ${purchaseDetails.purchaseID}');
+      String serverKey = purchaseDetails.verificationData.serverVerificationData
+          .replaceAll(RegExp(' +'), ' ');
+      log('SERVER VERIFICATION:: $serverKey');
+      // log('LOCAL VERIFICATION::: ${purchaseDetails.verificationData.localVerificationData}');
+      // log('SERVER VERIFICATION:::: ${purchaseDetails.verificationData.serverVerificationData}');
+      // log('TRANSACTION DATE:::; ${purchaseDetails.transactionDate.toString()}');
+      // log('PRODUCT ID::::;; ${purchaseDetails.productID}');
+      // log('PRODUCT ID::::;; ${purchaseDetails.purchaseID}');
       int response = await createSubscripton(receipt: serverKey);
       if (response == 200 || response == 201) {
         //TODO: Subscription routing logic
@@ -91,32 +66,10 @@ class SubscriptionController extends GetxController {
         Get.showSnackbar(
             Ui.ErrorSnackBar(message: 'An error occured please try again'));
       }
-
-      //     break;
-      //   case storeKeyConsumable:
-      //     print('Consumable done');
-      //     // counter.addBoughtDashes(1000);
-      //     break;
-      // }
-      // }
     }
     if (purchaseDetails.pendingCompletePurchase) {
       await iaPurchase.completePurchase(purchaseDetails);
     }
-  }
-
-  //TODO: Verify purchase
-
-  Future<void> _verifyPurchase(PurchaseDetails purchaseDetails) async {
-    // var functions = await firebaseNotifier.functions;
-    // final callable = functions.httpsCallable('verifyPurchase');
-    // final results = await callable({
-    //   'source': purchaseDetails.verificationData.source,
-    //   'verificationData':
-    //       purchaseDetails.verificationData.serverVerificationData,
-    //   'productId': purchaseDetails.productID,
-    // });
-    // return results.data as bool;
   }
 
   void _updateStreamOnDone() {
@@ -130,13 +83,11 @@ class SubscriptionController extends GetxController {
   Future<List<ProductDetails>?> loadPurchases() async {
     isLoading = true;
     update([kProductDetailBuilder]);
-    log('IN LOAD PURCHASES');
     products = List.empty(growable: true);
     Set<String> ids;
     log('IN LOAD PURCHASES ${products.length}');
     final available = await iaPurchase.isAvailable();
     if (!available) {
-      print('Not availalbe');
       storeState = StoreState.notAvailable;
       return null;
     }
@@ -147,13 +98,10 @@ class SubscriptionController extends GetxController {
     }
     products.addAll(response.productDetails);
     storeState = StoreState.available;
-    log('IN LOAD PURCHASES ${products.length}');
     isLoading = false;
     update([kProductDetailBuilder]);
     return products;
   }
-
-  //TODO: CHECK, DELETE AND POST SUBSCRIPTION
 
   Future<int> getSubscription() async {
     //TODO: save Subscription details
